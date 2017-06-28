523b9351a424c98aa27dc6489d35fdff88e9f11a
Patrick Kolodgy <pkolodgy@gmail.com>
2017-06-28 17:04:37 -0400

    Search btn sends input val

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -3,10 +3,12 @@ $(document).ready(function() {
   $('.search-bar').on('click', '.search-btn', function(e) {
     e.preventDefault();
 
+    var q = $(this).siblings('.search-field').val()
+
     $.ajax({
       url: '/search',
       method: 'POST',
-      data: 'q=hello'
+      data: `q=${q}`
     }).done(function(res) {
       console.log(res)
     });
