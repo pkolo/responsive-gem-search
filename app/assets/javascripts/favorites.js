7eac21c8c53e23cdbe0299b089441159c18f06e0
Patrick Kolodgy <pkolodgy@gmail.com>
2017-06-29 09:57:03 -0400

    Manages state in localstorage

--- app/assets/javascripts/favorites.js
+++ app/assets/javascripts/favorites.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     var gemName = gem[0].innerText;
 
     if ( localStorage.getItem('favorites') ) {
-      addFav(gem)
+      toggleFav(gem)
     } else {
       initFavs(gem);
     }
@@ -21,6 +21,18 @@ $(document).ready(function() {
     localStorage.setItem('favorites', JSON.stringify(favGems))
   }
 
+  var toggleFav = function(gem) {
+    var gemName = gem[0].innerText;
+    var favorites = JSON.parse(localStorage.getItem('favorites'));
+    var match = favorites.find( fav => (fav.name == gemName) )
+
+    if (match) {
+      removeFav(gemName)
+    } else {
+      addFav(gem)
+    }
+  }
+
   var addFav = function(gem) {
     var gemName = gem[0].innerText;
     var gemLink = gem.attr('href');
@@ -29,13 +41,18 @@ $(document).ready(function() {
     favorites.push(gemData)
 
     localStorage.setItem('favorites', JSON.stringify(favorites));
-    console.log(gemName, 'added')
-    console.log( favorites )
+    console.log(gemName, 'added');
+    console.log( favorites );
   }
 
   var removeFav = function(gemName) {
-    localStorage.removeItem(gemName);
+    var favorites = JSON.parse(localStorage.getItem('favorites'));
+    var newFavorites = favorites.filter( fav => ( fav.name != gemName) );
+
+    localStorage.setItem('favorites', JSON.stringify(newFavorites)) ;
+
     console.log(gemName, 'removed');
+    console.log( newFavorites );
   }
 
 });
