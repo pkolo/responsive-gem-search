eda564b6b919db28a8a515fb383af365ea264112
Patrick Kolodgy <pkolodgy@gmail.com>
2017-06-29 09:29:22 -0400

    Star btn adds fav to localStorage

--- app/assets/javascripts/favorites.js
+++ app/assets/javascripts/favorites.js
@@ -3,20 +3,34 @@ $(document).ready(function() {
   $('.search-wrapper').on('click', '.fav-btn', function(e) {
     var gem = $(this).siblings('.result-link');
     var gemName = gem[0].innerText;
-    if ( localStorage.getItem(gemName) ) {
-      removeFav(gemName)
-    } else {
-      addFav(gem)
-    }
-  })
 
+    if ( localStorage.getItem('favorites') ) {
+      addFav(gem)
+    } else {
+      initFavs(gem);
+    }
+  });
+
+  var initFavs = function(gem) {
+    var gemName = gem[0].innerText;
+    var gemLink = gem.attr('href');
+    var favGems = [
+      {name: gemName, link: gemLink}
+    ]
+
+    localStorage.setItem('favorites', JSON.stringify(favGems))
+  }
 
   var addFav = function(gem) {
     var gemName = gem[0].innerText;
     var gemLink = gem.attr('href');
+    var gemData = {name: gemName, link: gemLink};
+    var favorites = JSON.parse(localStorage.getItem('favorites'));
+    favorites.push(gemData)
 
-    localStorage.setItem(gemName, gemLink);
+    localStorage.setItem('favorites', JSON.stringify(favorites));
     console.log(gemName, 'added')
+    console.log( favorites )
   }
 
   var removeFav = function(gemName) {
