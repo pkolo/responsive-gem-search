1b51dce5fe463bd79b9ae3d989e5d911064edea2
Patrick Kolodgy <pkolodgy@gmail.com>
2017-06-29 12:28:08 -0400

    Favorites flagged in search result

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -10,9 +10,24 @@ $(document).ready(function() {
       method: 'POST',
       data: `q=${q}`
     }).done(function(res) {
-      $('.result-wrapper').remove()
-      $('.search-wrapper').append(res)
+      $('.result-wrapper').remove();
+      $('.search-wrapper').append(res);
+
+      var results = $('.search-wrapper').find('.result');
+
+      $.each(results, function(i, dependency) {
+        var gemName = $.trim(dependency.innerText);
+        var favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+        var isFav = favorites.find( fav => (fav.name == gemName) )
+
+        if (isFav) {
+          $(dependency).find('.fav-btn').attr('src', '/assets/star-blue.png')
+        }
+
+      });
+
     });
+
   });
 
 });
